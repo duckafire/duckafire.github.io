de9560ddf2afbf9e1ae6085cfa6ab6b119c51fa6
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-03-16 17:46:25 -0300

    Add 'show more' popup (in header)

---
+++ javascript/events/open-close.js
@@ -0,0 +1,8 @@
+const header = {
+	welcome: {
+		showMore: document.querySelector("section#header > section#welcome > div#top > div#show-more > i#show-more"),
+	},
+	showMore: document.querySelector("section#header > section#show-more"),
+}
+
+header.welcome.showMore.addEventListener("click", () => { header.showMore.hidden = false; });
