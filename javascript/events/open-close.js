95ded1356173bd8c584162381cd0b9db738e32a1
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-03-17 11:52:38 -0300

    Enable close button of the 'show more' popup (in header)

--- javascript/events/open-close.js
+++ javascript/events/open-close.js
@@ -1,12 +1,12 @@
 {
 
 const header = {
-	welcome: {
-		showMore: document.querySelector("section#header > section#welcome > div#top > div#show-more > i#show-more"),
-	},
+	openShowMore: document.querySelector("section#header > section#welcome > div#top > div#show-more > i#show-more"),
+	closeShowMore: document.querySelector("section#header > section#show-more > div#popup > div#top > i#close-popup"),
 	showMore: document.querySelector("section#header > section#show-more"),
-}
+};
 
-header.welcome.showMore.addEventListener("click", () => { header.showMore.hidden = false; });
+header.openShowMore.addEventListener("click", () => { header.showMore.hidden = false; });
+header.closeShowMore.addEventListener("click", () => { header.showMore.hidden = true; });
 
 }
