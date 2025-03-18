63e31539e5ee1cbe82ab6018565b560fe4a397b1
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-03-18 09:36:19 -0300

    Rename tag indexes

--- javascript/events/open-close.js
+++ javascript/events/open-close.js
@@ -1,12 +1,12 @@
 {
 
 const header = {
-	openShowMore: document.querySelector("section#header > section#welcome > div#top > div#show-more > i#show-more"),
-	closeShowMore: document.querySelector("section#header > section#show-more > div#popup > div#top > i#close-popup"),
-	showMore: document.querySelector("section#header > section#show-more"),
+	openPopup: document.querySelector("section#header > section#presentation > div#header > div#open-popup > i"),
+	closePopup: document.querySelector("section#header > section#popup > div#container > div#header > i#close-popup"),
+	popup: document.querySelector("section#header > section#popup"),
 };
 
-header.openShowMore.addEventListener("click", () => { header.showMore.hidden = false; });
-header.closeShowMore.addEventListener("click", () => { header.showMore.hidden = true; });
+header.openPopup.addEventListener("click",  () => { header.popup.style.display = "initial"; });
+header.closePopup.addEventListener("click", () => { header.popup.style.display = "none";    });
 
 }
--- javascript/start-dom.js
+++ javascript/start-dom.js
@@ -1,20 +1,20 @@
 {
 
-const knowledgeTechnolog = [
-	document.querySelector("section#header > section#show-more > div#popup > div#knowledge > div > div > section#programming-languages"),
-	document.querySelector("section#header > section#show-more > div#popup > div#knowledge > div > div > section#markup-languages"),
-	document.querySelector("section#header > section#show-more > div#popup > div#knowledge > div > div > section#other-utilities"),
+const knowTech = [
+	document.querySelector("section#header > section#popup > div#container > div#knowledge > div > div > section#programming-languages"),
+	document.querySelector("section#header > section#popup > div#container > div#knowledge > div > div > section#markup-languages"),
+	document.querySelector("section#header > section#popup > div#container > div#knowledge > div > div > section#other-utilities"),
 ];
 
 let foo;
 const max = [9, 3, 15];
 
-for(let knowId = 0; knowId < knowledgeTechnolog.length; knowId++){
+for(let knowId = 0; knowId < knowTech.length; knowId++){
 	for(let i = 0; i < max[knowId]; i++){
 		foo = document.createElement("img");
-		foo.className = "knowledge-technolog-image";
+		foo.className = "know-tech";
 		foo.src = "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg";
-		knowledgeTechnolog[knowId].appendChild(foo);
+		knowTech[knowId].appendChild(foo);
 	}
 }
 
