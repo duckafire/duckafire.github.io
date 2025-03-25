b0c8fee924d1693590e733b33bec605a222645a8
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-03-25 15:30:52 -0300

    Add known technologies to know-dialog

---
+++ javascript/constants.js
@@ -0,0 +1,7 @@
+const JSON_URL = {
+	knowTech: "https:raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/data-json/header/welcome-popup-know.json",
+	projectsItem: [
+		"https://raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/data-json/projects/highlight.json",
+		"https://raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/data-json/projects/other.json",
+	],
+}
---
+++ javascript/dom/from-json/know-tech.js
@@ -0,0 +1,38 @@
+{
+
+let figure, img;
+
+function newItem(data, level, dest){
+	figure = document.createElement("figure");
+	
+	img = document.createElement("img");
+	img.className = "know-tech-img " + level;
+	img.alt   = data.alt;
+	img.title = data.title;
+	img.src   = data.src;
+
+	console.log(img.className);
+	figure.appendChild(img);
+	dest.appendChild(figure);
+}
+
+fetch(JSON_URL.knowTech).then(response => {return response.json()}).then((json) => {
+	let level = ["low", "middle", "high"];
+	let tags  = ["programmingLanguages", "markupLanguages", "otherUtilities"];
+	let ids   = ["programming-lang", "markup-lang", "other-util"];
+
+	for(let i = 0; i < 3; i++){
+		for(const data of json[tags[i]]){
+			newItem(
+				data.icon,
+				"know-level-" + level[data.level - 1],
+				document.querySelector(`header > dialog#know-dialog > section > ul#knowledge > li > section#${ids[i]} > div`)
+			);
+		}
+	}
+
+});
+
+figure = img = undefined;
+
+}
