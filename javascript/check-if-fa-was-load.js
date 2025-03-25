c377efc52d03cc884752764086ed4140bc8bf819
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-03-25 16:49:29 -0300

    Add JS to check when Font Awesome is loaded:
    Used to removed 'wildcard' icon

---
+++ javascript/check-if-fa-was-load.js
@@ -0,0 +1,18 @@
+{
+
+let item = window.getComputedStyle(document.querySelector(".fa-github"));
+
+let checker = setInterval(() => {
+
+	if(item.fontFamily.includes("Font Awesome")){
+		document.querySelectorAll(".FA-wildcard").forEach((elem) => {
+			elem.classList.remove("FA-wildcard");
+		});
+
+		clearInterval(checker);
+		item = checker = undefined;
+	}
+
+}, 250);
+
+}
