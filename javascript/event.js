31fed0e7c79e188f0bef10c7be33bae487e3c32f
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-03-27 15:27:21 -0300

    Add event to expand 'other projects items' (on a button)

--- javascript/event.js
+++ javascript/event.js
@@ -1,11 +1,13 @@
 {
 
+let foo;
+
 // open/close dialog
 
 const DIALOG = {
 	know: document.querySelector("header > dialog#know-dialog"),
-	// "viewer" is opened/closed by
-	// projects items showcase images
+	// "viewer" is opened/closed by projects items showcase images
+	other: document.querySelector("main > dialog#other"),
 }
 
 document.querySelector("header > nav > div#top > section#right > ul > li > button#open-know-dialog")
@@ -13,5 +15,13 @@ document.querySelector("header > nav > div#top > section#right > ul > li > butto
 
 document.querySelector("header > dialog#know-dialog > section > nav > button#close-know-dialog")
 	.addEventListener("click", () => {DIALOG.know.open = false})
+
+foo = document.querySelector("main > button");
+
+foo.addEventListener("click", () => {
+	DIALOG.other.open = !DIALOG.other.open;
+})
+
+foo = undefined;
 
 }
