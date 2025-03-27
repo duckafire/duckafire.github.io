9d9f557788dc366447385bebf8637099e4588621
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-03-27 15:57:57 -0300

    Fix scroll bug

--- javascript/event.js
+++ javascript/event.js
@@ -11,10 +11,16 @@ const DIALOG = {
 }
 
 document.querySelector("header > nav > div#top > section#right > ul > li > button#open-know-dialog")
-	.addEventListener("click", () => {DIALOG.know.open = true})
+	.addEventListener("click", () => {
+		document.body.style.overflow = "hidden";
+		DIALOG.know.open = true
+	})
 
 document.querySelector("header > dialog#know-dialog > section > nav > button#close-know-dialog")
-	.addEventListener("click", () => {DIALOG.know.open = false})
+	.addEventListener("click", () => {
+		DIALOG.know.open = false
+		document.body.style.overflow = "";
+	})
 
 foo = document.querySelector("main > button");
 
