8f3ef047803cfc41a34a57906556ea0372163bdd
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-04-12 16:23:01 -0300

    Rename subdirectory (data-json -> json

--- javascript/constants.js
+++ javascript/constants.js
@@ -1,8 +1,8 @@
 const JSON_URL = {
-	knowTech: "https:raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/data-json/header/welcome-popup-know.json",
+	knowTech: "https:raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/json/header/welcome-popup-know.json",
 	projectsItems: [
-		"https://raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/data-json/projects/highlight.json",
-		"https://raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/data-json/projects/other.json",
+		"https://raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/json/projects/highlight.json",
+		"https://raw.githubusercontent.com/duckafire/nest/refs/heads/work-in-progress/json/projects/other.json",
 	],
 }
 
