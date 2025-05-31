63a9fde50103c7e5db2d8b3461416b3e8e899794
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-05-31 17:05:19 -0300

    Apply "theme classes":
    2 - Rename classes:
        * .t-link-behavior   -> t-link
        * .gap-150-rem       -> .gap-one-h-fifty-rem
        * .font-size-150-rem ->.fsize-one-h-fifty-rem
    3 - Remove classes:
        * .generic-link-container
        * .generic-link
    4 - Edit "main link buttons" order.

--- js/components/social-links.js
+++ js/components/social-links.js
@@ -13,18 +13,18 @@ li
 
 const data_json = [
 	// a: link | i: className
-	["#", "fa-brands fa-github"],
-	["#", "fa-brands fa-linkedin"],
 	["#", "fa-brands fa-itch-io"],
+	["#", "fa-brands fa-linkedin"],
+	["#", "fa-brands fa-github"],
 ];
 
 const create = (data, noMobile) => {
 	const li = document.createElement("li");
 	const button = document.createElement("button");
-	const a = new_anchor("no-std-style generic-link title-fsize", data[0]);
+	const a = new_anchor("no-std-style", data[0]);
 	const i = document.createElement("i");
 
-	button.className = "no-std-style hover-grow active-shrink" + (noMobile ? " only-not-at-mobile" : "");
+	button.className = "no-std-style title-fsize t-icon-btn" + (noMobile ? " only-not-at-mobile" : "");
 	i.className = data[1];
 
 	a.appendChild(i)
