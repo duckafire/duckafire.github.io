ef61423d18acc269f3228d31b5dd568cb963f4f1
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-06-03 13:02:58 -0300

    Add code (CSS + JS) to move big titles in highlight cards

--- js/components/cards/create.js
+++ js/components/cards/create.js
@@ -1,14 +1,28 @@
 "use strict";
 { // start
 
-const normal = document.getElementById("cards-container");
-const highlight = document.getElementById("highlight-cards-container");
+const normal_container = document.getElementById("cards-container");
+const highlight_container = document.getElementById("highlight-cards-container");
 
 for(const data of DATA_JSON.cards){
-	normal.appendChild( BUILD_CARD.normal(data) );
+	normal_container.appendChild( BUILD_CARD.normal(data) );
 
 	if(data.highlight != null)
-		highlight.appendChild( BUILD_CARD.highlight(data) );
+		highlight_container.appendChild( BUILD_CARD.highlight(data) );
+}
+
+const highlight_card_width  = parseInt(getComputedStyle(document.querySelector(".highlight-card-title-container")).width);
+const highlight_cards_title = document.querySelectorAll(".highlight-card-title");
+let px;
+
+for(const title of highlight_cards_title){
+	if(title.scrollWidth > highlight_card_width){
+		title.style.paddingLeft = "1rem";
+		px = title.scrollWidth + highlight_card_width;
+		title.style.setProperty("--text-length", String(px) + "px");
+	}else{
+		title.style.animation = "none";
+	}
 }
 
 } // end
--- js/components/cards/data-json.js
+++ js/components/cards/data-json.js
@@ -15,8 +15,9 @@ DATA_JSON.cards = []
 const get_num = (max, min) => Math.floor( Math.random() * max ) + min + 1;
 const max = get_num(5, 5);
 
-let usedTech;
-let techMax;
+let usedTech, title = "foo";
+let techMax, titleMax;
+
 
 for(let i = 0; i < max; i++){
 	usedTech = [];
@@ -25,15 +26,22 @@ for(let i = 0; i < max; i++){
 	for(let j = 0; j < techMax; j++)
 		usedTech.push("*c-plain");
 
+	titleMax = get_num(20, 1);
+
+	for(let j = 0; j < titleMax; j++)
+		title += "foo";
+
 	DATA_JSON.cards.push(
 		new_card(
-			"foo" + i,
+			title + i,
 			"Lorem ipsum odor amet, consectetuer adipiscing elit. Potenti ultricies inceptos, quam facilisis aliquam vehicula quis. Laoreet proin magna inceptos senectus lorem. Penatibus etiam metus quam sodales nisi. Pellentesque mattis ligula habitasse sed eleifend massa hendrerit accumsan. Venenatis sed penatibus justo sagittis laoreet malesuada elementum. Auctor adipiscing conubia phasellus; aptent habitasse accumsan. Diam inceptos finibus magna imperdiet gravida molestie consequat himenaeos. Imperdiet suscipit natoque sem tellus ut; vel urna. Vulputate cubilia sodales risus at eget et sociosqu lacus commodo.",
 			"https://cdn.jsdelivr.net/gh/duckafire/nest@work-in-progress/resources/images/foo.svg",
 			usedTech,
 			get_num(3, 0) == 1 ? { home_page: { "link": "#", "icon": "fa-brands fa-github" } } : null
 		)
 	);
+
+	title = "foo";
 }
 /* TODO: dev code; it must be removed */
 
